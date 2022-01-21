3c9f2b1207435478b0dcd41f320fd056148efd8f
Denis Butsky <nthbutsky@gmail.com>
2022-01-21 04:03:47 +0200

    deploy

---
+++ precache-manifest.e932b9128f226d16c570a1cf328c8c2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b037702da45c2761ba50",
+    "url": "/convidera-test/css/app.32fd59e2.css"
+  },
+  {
+    "revision": "d56b8f83e81d57e6db4a4f3e9a499bd7",
+    "url": "/convidera-test/img/img-01.d56b8f83.jpg"
+  },
+  {
+    "revision": "5f75436685c6077216914331196f29b1",
+    "url": "/convidera-test/img/img-02.5f754366.jpg"
+  },
+  {
+    "revision": "96b19185478622d8c4d31e942de4c522",
+    "url": "/convidera-test/img/img-03.96b19185.jpg"
+  },
+  {
+    "revision": "1b826c5ce3a2f5c2e0bfab8b1294f6c6",
+    "url": "/convidera-test/img/logo.1b826c5c.jpg"
+  },
+  {
+    "revision": "fab7712d3c99401c6d105dd2b58aca9e",
+    "url": "/convidera-test/index.html"
+  },
+  {
+    "revision": "b037702da45c2761ba50",
+    "url": "/convidera-test/js/app.8238d574.js"
+  },
+  {
+    "revision": "b1dbf5b5190393dadb7c",
+    "url": "/convidera-test/js/chunk-vendors.f0d3957d.js"
+  },
+  {
+    "revision": "5282a221a100ac3576394b718203725b",
+    "url": "/convidera-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/convidera-test/robots.txt"
+  }
+]);
